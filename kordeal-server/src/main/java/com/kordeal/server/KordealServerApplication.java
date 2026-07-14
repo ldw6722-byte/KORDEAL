cd5fe6310435487a106cb80e128f9d3f1bd7ebc7
leed <ldw6722@gmail.com>
2026-07-14 18:57:33 +0900

    0714

---
+++ kordeal-server/src/main/java/com/kordeal/server/KordealServerApplication.java
@@ -0,0 +1,13 @@
+package com.kordeal.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KordealServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KordealServerApplication.class, args);
+	}
+
+}
---
+++ kordeal-server/src/test/java/com/kordeal/server/KordealServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kordeal.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KordealServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
